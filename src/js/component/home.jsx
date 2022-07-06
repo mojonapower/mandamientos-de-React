db338f5fdeef1bd01124d56e9b25387c86ffc60f
mojonapower <maca95@gmail.com>
2022-07-06 00:31:22 +0000

    mandamientos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,24 +3,45 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
-const Home = () => {
+function Home () {
+	const estilo ={
+		"backgroundColor": "black",
+		"color": "white"
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
+		<div className="text-center" style={estilo}>
+			<Saludar/>
 			<p>
 				<img src={rigoImage} />
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
+			<Mandamiento/>
+			
 		</div>
 	);
 };
 
+function Saludar(){
+	return <>
+	<h1>Los mandamientos de React :D! </h1>
+	<h2>debes obedecer</h2>
+	</>
+}
+//etiqueta vacía se llama fragment 
+function Mandamiento (){
+	return(
+		<ol>
+				<li>Todos los componentes retornan html</li>
+				<li>Todos los componentes retornan UNA etiqueta padre</li>
+				<li>Todos los componentes se definen CON PASCAL CASE (inician con letra mayuscula)</li>
+				<li>Todos los componentes creados en otro archivo deben ser exportados para ser utilizados donde uno desee</li>
+				<li>Todas las clases asociadas a una etiqueta se les llama className</li>
+				<li>Para insertar valores JS (variables, arreglos, objetos, etc...) dentro de un componente, debemos dejarlo dentro de "{}"</li>
+				<li> toda etiqueta que se abre, se debe cerrar</li>
+				
+			</ol>
+	)
+}
 export default Home;
